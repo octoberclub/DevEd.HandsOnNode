60d5ff23d98a41e83055c96c4641d31c0ed7ba7c
octoberclub <michelle.steele@15below.com>
2014-09-19 09:08:13 +0100

    Added node example

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿var express = require('express')
+  , http = require('http');
+
+var app = express(); 
+var server = http.createServer(app);
+
+app.get('/poem/getPoem/:text', function(req, res) {
+  res.type('application/json');
+  var text = req.params.text;
+  console.log("Parameter: " + text);
+ 
+	poem = new Object();
+	poem.text = text;	
+	poem.lines = "Violets are blue";
+	
+	res.json(poem);
+});
+
+app.post('/poem/:from', function(req, res) {
+	var poem  = req.body;
+	console.log("Got request: " + JSON.stringify(poem));
+	console.log("from: " + poem.from);
+	res.send(poem);		
+});	
+ 
+app.listen(process.env.PORT || 9999);
+
+// curl -i -X GET -H 'Content-Type: application/json'  http://localhost:9999/poem/getPoem/Violets
